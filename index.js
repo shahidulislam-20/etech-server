9bad7ee16d76fd8a3881e81aa0e4a7ad411dd907
MD.SHAHIDUL ISLAM <shahidul.bd.1920@gmail.com>
2023-10-20 22:11:55 +0600

    input mongodb and setup it

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(express.json());
 
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zgm5tdq.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
