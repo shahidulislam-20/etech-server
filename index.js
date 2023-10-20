a9d6c4c3cb38cb9526a2c706479b8943363ce2cf
MD.SHAHIDUL ISLAM <shahidul.bd.1920@gmail.com>
2023-10-20 22:16:33 +0600

    create add to cart post api

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zgm5tdq.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
